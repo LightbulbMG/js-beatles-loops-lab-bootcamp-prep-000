168a1785f9e7669e3e58664a509bced078730460
Mercedes Gaillard <mdg816@gmail.com>
2019-05-29 12:20:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var b = [];
+  var play = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
+  var music = ["piano", "bass", "guitar", "drums"]
+  for(b = 0; b>)
+}
