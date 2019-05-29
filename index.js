4d7894f8bd73c3aaf17d9f352f1742abe1a846ee
Mercedes Gaillard <mdg816@gmail.com>
2019-05-29 12:23:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 function theBeatlesPlay(musicians, instruments){
   var b = [];
   var play = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
-  var music = ["piano", "bass", "guitar", "drums"]
-  for(b = 0; b <= 3; b++)
+  var music = ["guitar", "bass", "piano", "drums"]
+  for(b = 0; b <= 3; b++){
+    console.log("${play} plays ${music}")
+  }
 }
+
